95d8c607b63e7390fe6dfcafe486d63cf28f39fe
ollie <ollie@ollierobinson.co.uk>
2022-12-26 17:01:34 +0000

    added play wav example

---
+++ examples/play_wav.rs
@@ -0,0 +1,54 @@
+use mixr::{self, ChannelProperties, system::{AudioSystem}};
+use sdl2::audio::{AudioSpecDesired, AudioCallback};
+
+struct Audio<'a> {
+    system: &'a mut AudioSystem
+}
+
+impl<'a> AudioCallback for Audio<'a> {
+    type Channel = i16;
+
+    fn callback(&mut self, out: &mut [Self::Channel]) {
+        for x in out.iter_mut() {
+            *x = self.system.advance();
+        }
+    }
+}
+
+fn main() {
+    let path = std::env::args().nth(1).expect("A path is required.");
+    let path = path.as_str();
+
+    let mut system = AudioSystem::new(None, 1);
+
+    let pcm = mixr::loaders::PCM::load_wav(path).expect("A valid path is required. Make sure if it contains spaces, you surround it with quotes.");
+
+    let buffer = system.create_buffer();
+    system.update_buffer(buffer, &pcm.data, pcm.format).unwrap();
+
+    system.play_buffer(buffer, 0, ChannelProperties::default()).unwrap();
+
+    let sdl = sdl2::init().unwrap();
+    let audio = sdl.audio().unwrap();
+
+    let desired_spec = AudioSpecDesired {
+        freq: Some(system.format.sample_rate),
+        channels: Some(system.format.channels),
+        samples: Some(8192)
+    };
+
+    let device = audio.open_playback(None, &desired_spec, |_| {
+        Audio {
+            system: &mut system
+        }
+    }).unwrap();
+
+    device.resume();
+
+    ctrlc::set_handler(move || std::process::exit(0)).unwrap();
+
+    //std::thread::sleep(Duration::from_secs((((length as i32) / 4 / rate) - 1) as u64));
+    loop {
+        std::thread::sleep(std::time::Duration::from_secs(5));
+    }
+}
